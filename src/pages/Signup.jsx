7268d1c8716d80a926c402d91c493178f9833649
crisostomo_e <crisostomo_e@ca01.ub.lan>
2026-03-23 11:51:06 +0800

    Implemented event handling

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,41 @@
+import './Signup.css'
+import React, {
+    useState
+}
+
+from 'react';
+
 const Signup = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const onChangeName = (event) => {
+        setName(event.target.value);
+    };
+
+    const onChangeEmail = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const onSubmitForm = (event) => {
+        event.preventDefault();
+        console.log('Submitted:', { name, email })
+    };
+
     return (
-        <div>
-            <h1>Welcome to the Signup Page</h1>
-        </div>
+        <table>
+            <tr>
+                <td>
+                    <form onSubmit={onSubmitForm}>
+                        <label>Name:</label>
+                        <input type="text" value={name} onChange={onChangeName}></input> <br></br>
+                        <label>Email:</label>
+                        <input type="email" value={email} onChange={onChangeEmail}></input>
+                    </form>
+                </td>
+            </tr>
+        </table>
     );
 };
 
